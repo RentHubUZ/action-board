c226f0b2c00540a87e665aeeb68b7c6ba6280db4
Ozodbek2129 <ozodbekshamsiddinov1504@gmail.com>
2024-09-26 19:51:32 +0500

    all

--- migrations/000001_create_reviews_table.up.sql
+++ migrations/000001_create_reviews_table.up.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `reviews` (
-  `id` UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
-  `user_id` UUID NOT NULL, --buni tekshirish kerak emas ya'ni bu id tokendan keladi
-  `property_id` UUID NOT NULL,  --bu columni properties tabledan tekshirish kerak
-  `rating` INT,
-  `comment` TEXT,
-  `created_at` TIMESTAMP DEFAULT (NOW()),
-  `updated_at` TIMESTAMP DEFAULT (NOW())
+CREATE TABLE "reviews" (
+  "id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
+  "user_id" UUID NOT NULL, --buni tekshirish kerak emas ya'ni bu id tokendan keladi
+  "property_id" UUID NOT NULL,  --bu columni properties tabledan tekshirish kerak
+  "rating" INT,
+  "comment" TEXT,
+  "created_at" TIMESTAMP DEFAULT NOW(),
+  "updated_at" TIMESTAMP DEFAULT NOW()
 );
--- migrations/000002_create_favorites_table.up.sql
+++ migrations/000002_create_favorites_table.up.sql
@@ -1,6 +1,6 @@
-Table "favorites" (
-  "id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
+CREATE Table "favorites" (
+  "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   "user_id" UUID not null, --buni tekshirish kerak emas ya'ni bu id tokendan keladi
   "property_id" UUID not null, --bu columni properties tabledan tekshirish kerak
-  "created_at" TIMESTAMP DEFAULT (NOW())
+  "created_at" TIMESTAMP DEFAULT NOW()
 );
